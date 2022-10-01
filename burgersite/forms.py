de78e63273ded7c70fa8193a6e8726fcf84d3070
Карпович Кирилл ИСТбд-22 <99400515+GL1ntve1n@users.noreply.github.com>
2022-10-01 23:47:09 +0400

    BurgerSite
    BurgerSite

---
+++ burgersite/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        min_length=2,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Ваше имя', 'class': 'form-control valid'}
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'placeholder': 'E-mail', 'class': 'form-control valid'}
+        )
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'placeholder': 'Сообщение', 'cols': 30, 'rows': 9, 'class': 'form-control w-100'}
+        )
+    )
