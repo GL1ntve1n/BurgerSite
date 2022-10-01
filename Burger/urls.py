e79c855ffdba6ac4f01c227af3680d0148b8fd2e
Карпович Кирилл ИСТбд-22 <99400515+GL1ntve1n@users.noreply.github.com>
2022-10-01 23:37:34 +0400

    Add files via upload

---
+++ Burger/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from burgersite import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
